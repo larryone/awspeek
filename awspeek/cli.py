ff7b827817786b79851340ba3f10c4ce1d8016b6
Piotr Szymanski <skyleton@gmail.com>
2016-08-05 18:04:59 +0200

    possible targets in help desc

--- awspeek/cli.py
+++ awspeek/cli.py
@@ -3,7 +3,11 @@ import click
 
 @click.command()
 @click.option('--profile', default='dev')
-@click.option('--show', default='instance')
+@click.option(
+    '--show',
+    default='instance',
+    help='possible options: {}'.format(core.targets.keys())
+)
 def cli(profile, show):
     print core.show(profile, show)
 
